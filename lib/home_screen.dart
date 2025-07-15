8186e3fdbbf3f5dbc1453a2b535e7027596b15ff
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-07-16 02:21:06 +0600

    reset project

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:list_cart/student_model.dart';
 
 class home_screen extends StatefulWidget {
   const home_screen({super.key});
@@ -9,211 +9,33 @@ class home_screen extends StatefulWidget {
 }
 
 class _home_screenState extends State<home_screen> {
-
-  List<StudentModel> students =  [
-    StudentModel("S001", "Arif Hossain", "CSE", "3.85"),
-    StudentModel("S002", "Nusrat Jahan", "EEE", "3.67"),
-    StudentModel("S003", "Tanvir Ahmed", "BBA", "3.45"),
-    StudentModel("S004", "Maliha Rahman", "CSE", "3.92"),
-    StudentModel("S005", "Hasibul Hasan", "Civil", "3.33"),
-  ];
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List and Card", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-        backgroundColor: Colors.green,
+        title: Text("Bottom Nav, Card, List view", style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+        ),),
+        backgroundColor: Colors.blue,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            ListView.builder(
-                itemCount: students.length,
-                itemBuilder: (context, index){
-                  return Card(
-                    child: ListTile(
-                      title: Text("Name: ${students[index].name!}"),
-                      subtitle: Text("Department: ${students[index].department!}"),
-                      trailing: Text("CGPA: ${students[index].cgpa!}"),
-                    ),
-                  );
-                },
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              color: Colors.redAccent,
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan.", style: TextStyle(color: Colors.white),),
-                subtitle: Text("I am a software developer.", style: TextStyle(color: Colors.white),),
-                trailing: Icon(Icons.arrow_forward_ios, color: Colors.white,),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('K', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 20.0,
-                  backgroundColor: Colors.green,
-                  child: Text('H', style: TextStyle(color: Colors.white, fontSize: 20.5),),
-                ),
-                title: Text("MD.Kamrul Hasan."),
-                subtitle: Text("I am a software developer."),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            )
+          padding: EdgeInsets.all(8.50),
+          child: ListView(
+            children: [
+              ListTile(title: Text("List title"),)
+            ],
+          ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
 
-          ],
-        ),
+          ]
       ),
     );
   }
