10d3e3b47574af45d1e9ccdc9320daa09578b7a7
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2025-07-18 23:57:38 +0600

    Added bottom navigatin, ListView, ListView.builder, card, etc

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../student_model.dart';
+
 class home extends StatefulWidget {
   const home({super.key});
 
@@ -11,6 +13,19 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
 
+    List<StudentModel> students = [
+      StudentModel('1', 'MD. Kamrul Hasan', 'CSE', '3.31'),
+      StudentModel('2', 'Jasim Mia', 'CSE', '2.67'),
+      StudentModel('3', 'Fatema Akter', 'EEE', '3.85'),
+      StudentModel('4', 'Rahim Uddin', 'BBA', '3.45'),
+      StudentModel('5', 'Sadia Islam', 'CSE', '3.90'),
+      StudentModel('6', 'Nurul Amin', 'Mechanical', '2.95'),
+      StudentModel('7', 'Shamim Reza', 'CSE', '3.70'),
+      StudentModel('8', 'Nusrat Jahan', 'Civil', '3.20'),
+      StudentModel('9', 'Tanvir Hossain', 'CSE', '2.88'),
+      StudentModel('10', 'Rafiul Islam', 'EEE', '3.50'),
+    ];
+
 
 
     return Scaffold(
@@ -19,6 +34,32 @@ class _homeState extends State<home> {
         child: Center(
           child: ListView(
              children: [
+               Text(
+                 "List view builder.",
+                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.50),
+               ),
+               SizedBox(
+                 height: 400,
+                 child: ListView.builder(
+                     itemCount: students.length,
+                     itemBuilder: (context, index){
+                       StudentModel student = students[index];
+                       return Card(
+                         child: ListTile(
+                           leading: Text(student.id!),
+                           title: Text(
+                             "Student's name: ${student.name!}",
+                             style: TextStyle(
+                                 color: Colors.blue,
+                                 fontWeight: FontWeight.bold
+                             ),
+                           ),
+                           subtitle: Text("Department: ${student.department!}"),
+                           trailing: Text("CGPA: ${student.cgpa!}"),
+                         ),
+                       );
+                     }),
+               ),
                Text(
                  "List view.",
                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.50),
